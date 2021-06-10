91e43f55d9a202b12c0fd379ffefc3a4c888e766
Ryan Logsdon <ryan@ryanlogsdon.com>
2021-06-10 01:54:58 -0700

    created canvas

---
+++ sketch.js
@@ -0,0 +1,11 @@
+// much better debugging here: https://editor.p5js.org/
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);        // create a canvas using the screen's dimensions
+
+}
+
+function draw() {
+    background(255, 255, 255);                      // set the background color (RGB)
+}
