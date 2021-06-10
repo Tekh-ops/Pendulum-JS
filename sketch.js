4e8f1b8695d72f421bb92f586af9e0b352786cf2
Ryan Logsdon <ryan@ryanlogsdon.com>
2021-06-10 02:00:32 -0700

    added a pendulum class

--- sketch.js
+++ sketch.js
@@ -1,55 +1,65 @@
 // much better debugging here: https://editor.p5js.org/
 
-let pivotPoint;
-let r;
-let position;
-let angle;
-let gravity;
-let velocity;
-let acceleration;
-let damping;
-let diameter;
+
+
+let pendulums = [];
+let totalPendulums = 30;
+let shortestArmLength = 275;
+let armDelta = 20;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);    // create a canvas with all available space on screen
-
-    let x = width / 2;
-    let y = 0;
-    pivotPoint = createVector(x, y);
-    r = 500;
-    position = createVector();                 // this will track the position of the pendulum ball
-    angle = PI / 6;                            // starting angle of the pendulums
-    gravity = 0.4;
-
-    velocity = 0.0;
-    acceleration = 0.0;
-    damping = 1;                               // less than 1 will slow the pendulum down
-    diameter = 10;
+    createCanvas(windowWidth, windowHeight);            // create a canvas with all available space on screen
+    for (let i = 0; i < totalPendulums; i++) {
+        let x = width/2;
+        let y = 0;
+        let pendulumLength = shortestArmLength + (armDelta * i);
+        let p = new Pendulum(createVector(x, y), pendulumLength);
+        pendulums.push(p);
+    }
 }
 
 function draw() {
-    background(250, 250, 250);                  // set the background color (RGB)
+    background(0, 0, 0);                                // set the background color (RGB)
 
-    updatePosition();
-    display();
+    for (let i = 0; i < pendulums.length; i++) {
+        let p = pendulums[i];
+        p.updatePosition();
+        p.display();
+    }
 }
 
-function updatePosition() {
-    acceleration = (-1 * gravity / r) * sin(angle);
-    velocity += acceleration;
-    velocity *= damping;
-    angle += velocity;
+class Pendulum {
+    constructor(pivotPoint, armLength) {
+        this.pivotPoint = pivotPoint.copy();
+        this.position = createVector();                 // this will track the position of the pendulum ball
+        this.r = armLength;
+        this.angle = PI / 6;                            // starting angle of the pendulums
+        this.gravity = 0.4;
 
-    position.set(r * sin(angle), r * cos(angle));
-    position.add(pivotPoint);                   // add the pivot point to the pendulum ball's position
-}
+        this.velocity = 0.0;
+        this.acceleration = 0.0;
+        this.damping = 1;                               // less than 1 will slow the pendulum down
+        this.diameter = 10;                             // pendulum ball diameter
+    }
 
-function display() {
-    stroke(50, 50, 50);                         // pendulum arm color
-    strokeWeight(2);                            // pendulum arm width
-    line(pivotPoint.x, pivotPoint.y, position.x, position.y);
+    updatePosition() {
+        this.acceleration = (-1 * this.gravity / this.r) * sin(this.angle);
+        this.velocity += this.acceleration;
+        this.velocity *= this.damping;
+        this.angle += this.velocity;
 
-    ellipseMode(CENTER);
-    fill(0, 0, 250);                            // pendulum ball color
-    ellipse(position.x, position.y, diameter, diameter);
+        this.position.set(this.r * sin(this.angle), this.r * cos(this.angle));
+        this.position.add(this.pivotPoint);             // add the pivot point to the pendulum ball's position
+    }
+
+    display() {
+        // stroke(255, 255, 255);                          // pendulum arm color
+        // strokeWeight(2);                                // pendulum arm width
+        // line(this.pivotPoint.x, this.pivotPoint.y, this.position.x, this.position.y);
+        
+        ellipseMode(CENTER);
+        fill(250, 250, 250);                                // pendulum ball color
+        // fill(0, 0, 250);                                // pendulum ball color
+        ellipse(this.position.x, this.position.y, this.diameter, this.diameter);
+    }
 }
