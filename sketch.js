c4b3e08a3c1f350820f38aa83eafe50b6f7aba4f
Ryan Logsdon <ryan@ryanlogsdon.com>
2021-06-10 01:59:00 -0700

    static pendulum

--- sketch.js
+++ sketch.js
@@ -1,11 +1,34 @@
 // much better debugging here: https://editor.p5js.org/
 
+let pivotPoint;                                     // top attachment point
+let r;                                              // pendulum arm, r
+let position;                                       // (x, y) position of the pendulum ball
+let angle;                                          // angle of the swing
+let diameter;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);        // create a canvas using the screen's dimensions
+    createCanvas(windowWidth, windowHeight);        // create a canvas with all available space on screen
 
+    let x = width / 2;
+    let y = 0;
+    pivotPoint = createVector(x, y);
+    r = 500;
+    position = createVector();                      // this will track the position of the pendulum ball 
+    angle = PI / 6;                                 // starting angle of the pendulums
+    diameter = 10;
 }
 
 function draw() {
-    background(255, 255, 255);                      // set the background color (RGB)
+    background(250, 250, 250);                      // set the background color (RGB)
+
+    position.set(r * sin(angle), r * cos(angle));
+    position.add(pivotPoint);
+
+    stroke(50, 50, 50);                             // pendulum arm color
+    strokeWeight(2);                                // pendulum arm width
+    line(pivotPoint.x, pivotPoint.y, position.x, position.y);
+
+    ellipseMode(CENTER);
+    fill(0, 0, 250);                                // pendulum ball color
+    ellipse(position.x, position.y, diameter, diameter);
 }
